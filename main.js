783bad07065a87651ef0d2c2699ab0a6eee2caa4
wanghveganjerky <99945874+wanghveganjerky@users.noreply.github.com>
2023-11-02 20:35:11 -0400

    1

--- main.js
+++ main.js
@@ -1,35 +1,47 @@
-let lastTime = 0;
-let shakeThreshold = 15; // Adjust this value for sensitivity
-let lastX = 0, lastY = 0, lastZ = 0;
+let shakeThreshold = 20; // Adjust this value for sensitivity
+let lastX, lastY, lastZ;
 
-if (window.DeviceMotionEvent) {
-    window.addEventListener('devicemotion', shakeDetection, false);
+if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+    // iOS 13+
+    DeviceOrientationEvent.requestPermission()
+    .then(response => {
+        if (response == 'granted') {
+            window.addEventListener('devicemotion', shakeDetection, false);
+        }
+    })
+    .catch(console.error);
 } else {
-    alert("Sorry, your browser doesn't support DeviceMotion");
+    // non iOS 13+
+    window.addEventListener('devicemotion', shakeDetection, false);
 }
 
 function shakeDetection(event) {
     const acceleration = event.accelerationIncludingGravity;
-    const currentTime = new Date().getTime();
 
-    if ((currentTime - lastTime) > 100) {
-        const diffTime = currentTime - lastTime;
-        lastTime = currentTime;
-
-        const speed = Math.abs(acceleration.x + acceleration.y + acceleration.z - lastX - lastY - lastZ) / diffTime * 10000;
-
-        if (speed > shakeThreshold) {
-            triggerAnimation();
-        }
-
+    if (!lastX) {
         lastX = acceleration.x;
         lastY = acceleration.y;
         lastZ = acceleration.z;
+        return;
     }
+
+    const deltaX = Math.abs(acceleration.x - lastX);
+    const deltaY = Math.abs(acceleration.y - lastY);
+    const deltaZ = Math.abs(acceleration.z - lastZ);
+
+    if (deltaX + deltaY + deltaZ > shakeThreshold) {
+        duplicateBox();
+    }
+
+    lastX = acceleration.x;
+    lastY = acceleration.y;
+    lastZ = acceleration.z;
 }
 
-function triggerAnimation() {
+function duplicateBox() {
+    const container = document.body;
     const box = document.getElementById('box');
-    // Here, you can add any kind of animation or effect you want
-    box.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Change color randomly
+    const newBox = box.cloneNode(true);
+    newBox.id = ""; // Removing the id for the cloned box to keep ids unique
+    container.appendChild(newBox);
 }
